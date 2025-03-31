a36e9cb538c2c6d42c953acb0f30c37f24c063ca
Andrea <87487045+AndreaLumi@users.noreply.github.com>
2025-03-31 16:31:58 +0200

    Add files via upload

--- biblioteca/src/main/java/com/example/biblioteca/repository/BibliotecaRepository.java
+++ biblioteca/src/main/java/com/example/biblioteca/repository/BibliotecaRepository.java
@@ -1,12 +1,11 @@
 package com.example.biblioteca.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List; 
-import com.example.biblioteca.model.*;
 
-public class BibliotecaRepository {
-    interface BookRepository extends JpaRepository<BibliotecaRepository, String> {
-        List<BibliotecaRepository> findByGenere(String genere);
-        List<BibliotecaRepository> findByAnno(int anno);
-        List<BibliotecaRepository> findByDisponibilità(boolean disponibilità);
-    }
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import com.example.biblioteca.model.Biblioteca;
+
+public interface BibliotecaRepository extends JpaRepository<Biblioteca, String> {
+    List<Biblioteca> findByGenere(String genere);
+    List<Biblioteca> findByAnno(int anno);
+    List<Biblioteca> findByDisponibilità(boolean disponibilità);
 }
